6704ca3ac3277807f2ffe1348c2f703009fe23a8
eooce <142894633+eooce@users.noreply.github.com>
2024-01-27 19:12:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ cleanup_oldfiles() {
   rm -rf ${FILE_PATH}/boot.log ${FILE_PATH}/sub.txt ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yml
 }
 cleanup_oldfiles
-sleep 1
+sleep 2
 
 generate_config() {
   cat > ${FILE_PATH}/config.json << EOF
@@ -85,7 +85,6 @@ EOF
 generate_config
 sleep 2
 
-# 下载依赖
 ARCH=$(uname -m) && DOWNLOAD_DIR="${FILE_PATH}" && mkdir -p "$DOWNLOAD_DIR" && declare -a FILE_INFO 
 if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ]|| [ "$ARCH" == "aarch64" ]; then
     FILE_INFO=("https://github.com/eooce/test/releases/download/arm64/bot13 bot" "https://github.com/eooce/test/releases/download/ARM/web web" "https://github.com/eooce/test/releases/download/ARM/swith npm")
@@ -100,20 +99,19 @@ for entry in "${FILE_INFO[@]}"; do
     NEW_FILENAME=$(echo "$entry" | cut -d ' ' -f 2)
     FILENAME="$DOWNLOAD_DIR/$NEW_FILENAME"
     curl -L -sS -o "$FILENAME" "$URL"
-    echo "Downloading $FILENAME"
+    echo -e "\e[1;32mDownloading $FILENAME\e[0m"
 done
 wait
 for entry in "${FILE_INFO[@]}"; do
     NEW_FILENAME=$(echo "$entry" | cut -d ' ' -f 2)
     FILENAME="$DOWNLOAD_DIR/$NEW_FILENAME"
     chmod +x "$FILENAME"
-    echo "$FILENAME downloaded and permission successfully "
+    echo -e "\e[1;32m$FILENAME permission successfully\e[0m"
 done
 
-#生成固定隧道配置文件
 argo_configure() {
   if [[ -z $ARGO_AUTH || -z $ARGO_DOMAIN ]]; then
-    echo "ARGO_DOMAIN or ARGO_AUTH variable is empty, use quick tunnel"
+    echo -e "\e[1;32mARGO_DOMAIN or ARGO_AUTH variable is empty, use quick tunnels\e[0m"
     return
   fi
 
@@ -132,53 +130,56 @@ ingress:
   - service: http_status:404
 EOF
   else
-    echo "ARGO_AUTH mismatch TunnelSecret,use token connect to tunnel"
+    echo -e "\e[1;32mARGO_AUTH mismatch TunnelSecret,use token connect to tunnel\e[0m"
   fi
 }
 argo_configure
 sleep 2
 
 run() {
-  if [ -e "${FILE_PATH}/npm" ]; then
-    	tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
-    	if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
-    		NEZHA_TLS="--tls"
-    	else
-    		NEZHA_TLS=""
-    	fi
+  if [ -e ${FILE_PATH}/npm ]; then
+    chmod 777 ${FILE_PATH}/npm 
+    tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
+    if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
+      NEZHA_TLS="--tls"
+    else
+      NEZHA_TLS=""
+    fi
     if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
         nohup ${FILE_PATH}/npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
-        sleep 2
-        pgrep -x "npm" > /dev/null && echo "npm is running" || { echo "npm is not running, restarting..."; pkill -x "npm" && nohup ${FILE_PATH}/npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; echo "npm restarted"; }
+		    sleep 1
+        pgrep -x "npm" > /dev/null && echo -e "\e[1;32mnpm is running\e[0m" || { echo -e "\e[1;35mnpm is not running, restarting...\e[0m"; pkill -x "npm" && nohup ${FILE_PATH}/npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32mnpm restarted\e[0m"; }
     else
-        echo "NEZHA variable is empty,skip runing"
+        echo -e "\e[1;35mNEZHA variable is empty,skiping runing\e[0m"
     fi
   fi
 
-  if [ -e "${FILE_PATH}/web" ]; then
+  if [ -e ${FILE_PATH}/web ]; then
+    chmod 777 ${FILE_PATH}/web
     nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
-    sleep 2
-    pgrep -x "web" > /dev/null && echo "web is running" || { echo "web is not running, restarting..."; pkill -x "web" && nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 & sleep 2; echo "web restarted"; }
-
+	  sleep 2
+    pgrep -x "web" > /dev/null && echo -e "\e[1;32mweb is running\e[0m" || { echo "web is not running, restarting..."; pkill -x "web" && nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 & sleep 2; eecho -e "\e[1;32mweb restarted\e[0m"; }
   fi
 
-  if [ -e "${FILE_PATH}/bot" ]; then
+  if [ -e ${FILE_PATH}/bot ]; then
+    chmod 777 ${FILE_PATH}/bot
     if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
-      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
+    args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
     elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
-      args="tunnel --edge-ip-version auto --config ${FILE_PATH}/tunnel.yml run"
+    args="tunnel --edge-ip-version auto --config ${FILE_PATH}/tunnel.yml run"
     else
-      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:$ARGO_PORT"
+    args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:$ARGO_PORT"
     fi
     nohup ${FILE_PATH}/bot $args >/dev/null 2>&1 &
-    sleep 3
-    pgrep -x "bot" > /dev/null && echo "bot is running" || { echo "bot is not running, restarting..."; pkill -x "bot" && nohup ${FILE_PATH}/bot $args >/dev/null 2>&1 & sleep 2; echo "bot restarted"; }
+    pgrep -x "bot" > /dev/null && echo -e "\e[1;32mbot is running\e[0m" || { echo -e "\e[1;35mbot is not running, restarting...\e[0m"; pkill -x "bot" && nohup ${FILE_PATH}/bot $args >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32mbot restarted\e[0m"; }
+	  sleep 3
   fi
 } 
 run
+sleep 3
 
 function get_argodomain() {
-  if [[ -n "$ARGO_AUTH" ]]; then
+  if [[ -n $ARGO_AUTH ]]; then
     echo "$ARGO_DOMAIN"
   else
     cat ${FILE_PATH}/boot.log | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}'
@@ -187,8 +188,8 @@ function get_argodomain() {
 
 generate_links() {
   argodomain=$(get_argodomain)
-  sleep 3
-  echo "Argodomain:$argodomain"
+  echo -e "\e[1;32mArgodomain:\e[1;35m${argodomain}\e[0m"
+  sleep 2
 
   isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
   sleep 2
@@ -205,13 +206,13 @@ EOF
 
   base64 -w0 ${FILE_PATH}/list.txt > ${FILE_PATH}/sub.txt
   cat ${FILE_PATH}/sub.txt
-  echo -e "\nFile saved successfully"
+  echo -e "\n\e[1;32m${FILE_PATH}/sub.txt saved successfully\e[0m"
   sleep 8  
-  rm -rf ${FILE_PATH}/list.txt${FILE_PATH}/boot.log ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yml
+  rm -rf ${FILE_PATH}/list.txt ${FILE_PATH}/boot.log ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yml
 }
 generate_links
 sleep 15
 clear
 
-echo "Server is running"
-echo "Thank you for using this script,enjoy!"
+echo -e "\e[1;32mserver is running\e[0m"
+echo -e "\e[1;32mThank you for using this script,enjoy!\e[0m"
